3be98b9c068daf99275b042a51d759888d4c21eb
Alice <alice@tymate.com>
2022-06-13 14:04:39 +0200

    feat: deleting avo & setup admin trestle

--- app/controllers/avo/bookings_controller.rb
+++
@@ -1,2 +0,0 @@
-class Avo::BookingsController < Avo::ResourcesController
-end
--- config/initializers/trestle.rb
+++ config/initializers/trestle.rb
@@ -3,7 +3,7 @@ Trestle.configure do |config|
   #
   # Set the page title shown in the main header within the admin.
   #
-  config.site_title = "Projet"
+  config.site_title = "Studio Yoga"
 
   # Specify a custom image to be used in place of the site title for mobile and
   # expanded/desktop navigation. These images should be placed within your
@@ -33,7 +33,7 @@ Trestle.configure do |config|
   #
   # Set the path at which to mount the Trestle admin. Defaults to /admin.
   #
-  # config.path = "/admin"
+  config.path = "/admin"
 
   # Toggle whether Trestle should automatically mount the admin within your
   # Rails application's routes. Defaults to true.
