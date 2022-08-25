4590a8fec039b32b94784c79fc6e163c3ad5af5d
Julien <julien.camblan@gmail.com>
2022-08-25 21:56:40 +0200

    fix: remove sprocket

--- app/controllers/yoga_sessions_controller.rb
+++ app/controllers/yoga_sessions_controller.rb
@@ -9,12 +9,12 @@ class YogaSessionsController < ApplicationController
     @yoga_session = YogaSession.find(params[:id])
     id = params[:id].to_i
 
-    if session[:cart].include?(id) 
+    if session[:cart].include?(id)
       respond_to do |format|
+        format.turbo_stream
         format.html { redirect_to yoga_sessions_path, notice: 'Yoga session was already added to cart.' }
-        format.turbo_stream { flash.now[:notice] = 'Yoga session was already added to cart.' }
       end
-    else 
+    else
       @session = session[:cart] << id
       respond_to do |format|
         format.html { redirect_to yoga_sessions_path, notice: 'Yoga session was added to cart.' }
@@ -23,7 +23,7 @@ class YogaSessionsController < ApplicationController
     end
   end
 
-  def remove_from_cart #TO DO rajouter msg d'info
+  def remove_from_cart # TO DO rajouter msg d'info
     id = params[:id].to_i
     session[:cart].delete(id)
     redirect_to cart_index_path
