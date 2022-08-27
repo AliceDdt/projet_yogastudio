d90f8d7b8a1e3310a29575937b15fc9f4eae1c72
Alice <alice@tymate.com>
2022-08-27 18:05:47 +0200

    feat: delete unused js

--- app/javascript/cart.js
+++
@@ -1,9 +0,0 @@
-import BookingCart from './modules/BookingCart.js'
-
-document.addEventListener('DOMContentLoaded', () => {
-	let myBooking = new BookingCart();
-	myBooking.restore();
-	myBooking.showCart();
-	myBooking.removeFromCart();
-
-});
