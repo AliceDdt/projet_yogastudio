0455136b6d1311fd00ab1f89c488c4c5d72703c0
Alice <alice@tymate.com>
2022-08-28 12:45:57 +0200

    fix: turbo stream bug

--- app/controllers/yoga_sessions_controller.rb
+++ app/controllers/yoga_sessions_controller.rb
@@ -29,28 +29,4 @@ class YogaSessionsController < ApplicationController
     session[:cart].delete(id)
     redirect_to cart_index_path
   end
-
-  def add_to_cart
-    @yoga_session = YogaSession.find(params[:id])
-    id = params[:id].to_i
-
-    if session[:cart].include?(id)
-      respond_to do |format|
-        format.html { redirect_to yoga_sessions_path, notice: 'Yoga session was already added to cart.' }
-        format.turbo_stream { flash.now[:notice] = 'Yoga session was already added to cart.' }
-      end
-    else
-      @session = session[:cart] << id
-      respond_to do |format|
-        format.html { redirect_to yoga_sessions_path, notice: 'Yoga session was added to cart.' }
-        format.turbo_stream { flash.now[:notice] = 'Yoga session was added to cart.' }
-      end
-    end
-  end
-
-  def remove_from_cart # TO DO rajouter msg d'info
-    id = params[:id].to_i
-    session[:cart].delete(id)
-    redirect_to cart_index_path
-  end
 end
