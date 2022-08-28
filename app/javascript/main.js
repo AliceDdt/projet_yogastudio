093a34a37f447328222d7f30d335c07513170b04
Alice <alice@tymate.com>
2022-08-28 11:41:43 +0200

    feat: add js for navbar & flash message

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -2,10 +2,6 @@ import Page from './modules/Page.js'
 
 document.addEventListener('DOMContentLoaded', function () {
 
-	let myBooking = new BookingCart();
-	myBooking.restore();
-	myBooking.setTotalCart();
-
 	let myPage = new Page('.navbar__btn', '.navbar', '.main');
 	myPage.animateSideNav();
 	myPage.displayAlert('.alert');
