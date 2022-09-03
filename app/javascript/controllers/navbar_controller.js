2fcab149257e8d0778fc2b22f9c5a6b25d983997
Alice <alice@tymate.com>
2022-09-03 16:48:22 +0200

    refactor: remove unused js

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -4,15 +4,6 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
 	static targets = [ 'toggable' ];
 
-	// connect() {
-  //   useClickOutside(this)
-  // }
-
-  // clickOutside(event) {
-  //   this.toggableTarget.classList.remove('open');
-  // }
-
-
 	animateNav() {
 		 this.toggableTarget.classList.toggle('open');
 	}
